62a4182c30587e3529b6e64f73fe84fd956ea5fa
snyvvZ <snyvv@naver.com>
2021-09-17 20:26:22 +0900

    Maintab Component

--- src/components/common/MainTab.js
+++ src/components/common/MainTab.js
@@ -4,22 +4,43 @@ import Update from "../pages/Update";
 import Coordi from "../pages/Coordi";
 import Ranking from "../pages/Ranking";
 import styled from "styled-components";
+import variables from "../../styles/variables";
 
 const StyledNavigation = styled.nav`
   display: flex;
+  overflow: hidden;
+  margin: 30px 15px 15px 15px;
+  border-radius: 6px;
+
+  > a {
+    display: flex;
+    flex: 1 1;
+    align-items: center;
+    justify-content: center;
+    height: 40px;
+    color: ${variables.gray600};
+    background-color: ${variables.gray200};
+    border: 1px solid ${variables.gray200};
+    box-sizing: border-box;
+
+    &.is-active {
+      color: ${variables.black};
+      background-color: ${variables.white};
+    }
+  }
 `;
 
 const MainTab = () => {
   return (
     <>
       <StyledNavigation>
-        <NavLink to="/" activeClassName="is-active">
+        <NavLink exact to="/" activeClassName="is-active">
           업데이트
         </NavLink>
-        <NavLink to="/coordi" activeClassName="is-active">
+        <NavLink exact to="/coordi" activeClassName="is-active">
           코디
         </NavLink>
-        <NavLink to="/ranking" activeClassName="is-active">
+        <NavLink exact to="/ranking" activeClassName="is-active">
           랭킹
         </NavLink>
       </StyledNavigation>
